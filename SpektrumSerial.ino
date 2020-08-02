14885ece31c8b27726e34a386faf392fa1b3e410
daniel-relabs <daniel@relabs.io>
2020-08-02 08:33:53 +0200

    Working set for displaying on Heltec Wifi 32 OLED

--- SpektrumSerial.ino
+++ SpektrumSerial.ino
@@ -1,102 +1,171 @@
-/*
-  SerialPassthrough sketch
 
-  Some boards, like the Arduino 101, the MKR1000, Zero, or the Micro, have one
-  hardware serial port attached to Digital pins 0-1, and a separate USB serial
-  port attached to the IDE Serial Monitor. This means that the "serial
-  passthrough" which is possible with the Arduino UNO (commonly used to interact
-  with devices/shields that require configuration via serial AT commands) will
-  not work by default.
-
-  This sketch allows you to emulate the serial passthrough behaviour. Any text
-  you type in the IDE Serial monitor will be written out to the serial port on
-  Digital pins 0 and 1, and vice-versa.
-
-  On the 101, MKR1000, Zero, and Micro, "Serial" refers to the USB Serial port
-  attached to the Serial Monitor, and "Serial1" refers to the hardware serial
-  port attached to pins 0 and 1. This sketch will emulate Serial passthrough
-  using those two Serial ports on the boards mentioned above, but you can change
-  these names to connect any two serial ports on a board that has multiple ports.
-
-  created 23 May 2016
-  by Erik Nyquist
-*/
+#include <Arduino.h>
+#include <HardwareSerial.h>
+#include "heltec.h"
 
 char *inputString = 0;
 char foundHeader = 0;
+char message[16];
+unsigned char buffer[64]; // buffer array for data receive over serial port
+int count = 0;
+char temp = 0;
+
+typedef struct
+{
+  char fades;
+  char system;
+  int servo[7];
+
+} formatted_message;
+
+formatted_message last;
+
+void clearBufferArray() // function to clear buffer array
+{
+  for (int i = 0; i < count; i++)
+  {
+    buffer[i] = NULL;
+  } // clear all index of array with command NULL
+}
 
 void setup()
 {
   Serial.begin(115200);
+  Serial.println("this is a test");
   Serial1.begin(115200);
-  pinMode(14, OUTPUT);
-  digitalWrite(14, LOW);
+  Serial2.begin(9600);
+  pinMode(17, OUTPUT);
+  Heltec.display->init();
+  Heltec.display->flipScreenVertically();
+  Heltec.display->setFont(ArialMT_Plain_10);
+  Heltec.display->setTextAlignment(TEXT_ALIGN_CENTER);
+  Heltec.display->drawString(64, 0, "Spektrum Tests");
+  Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+  Heltec.display->drawString(0, 10, "Fad");
+  //Heltec.display->drawString(20, 10, String(45));
+  Heltec.display->drawString(64, 10, "Sys");
+  Heltec.display->drawString(0, 20, "Ch");
+  Heltec.display->drawString(64, 20, "Ch");
+  Heltec.display->drawString(0, 30, "Ch");
+  Heltec.display->drawString(64, 30, "Ch");
+  Heltec.display->drawString(0, 40, "Ch");
+  Heltec.display->drawString(64, 40, "Ch");
+  Heltec.display->drawString(0, 50, "Ch");
+  Heltec.display->display();
 }
 
 void loop()
 {
-  if (foundHeader)
-  {
-    ///Test
-    digitalWrite(14, HIGH);
-    delayMicroseconds(40);
-    digitalWrite(14, LOW);
-    foundHeader = FALSE;
-  }
-}
 
-void serialEvent1()
-{
+  char chain_id;
+  char chan_value;
 
-  while (Serial1.available())
+  //Read the Serial port and process
+
+  char index = 0;
+
+  //Update the display
+  Heltec.display->clear();
+  Heltec.display->setFont(ArialMT_Plain_10);
+  Heltec.display->setTextAlignment(TEXT_ALIGN_CENTER);
+  Heltec.display->drawString(64, 0, "Spektrum Tests");
+  Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+  Heltec.display->drawString(0, 10, "Fad");
+  Heltec.display->drawString(64, 10, "Sys");
+  Heltec.display->drawString(0, 20, "Ch");
+  Heltec.display->drawString(64, 20, "Ch");
+  Heltec.display->drawString(0, 30, "Ch");
+  Heltec.display->drawString(64, 30, "Ch");
+  Heltec.display->drawString(0, 40, "Ch");
+  Heltec.display->drawString(64, 40, "Ch");
+  Heltec.display->drawString(0, 50, "Ch");
+  Heltec.display->drawString(20, 10, String(last.fades, DEC));
+  Heltec.display->drawString(84, 10, String(last.system, HEX));
+  Heltec.display->drawString(20, 20, String((last.servo[0] & 0x7800) / 2048, DEC));
+  Heltec.display->drawString(30, 20, String((last.servo[0] & 0x07FF), DEC));
+  Heltec.display->drawString(84, 20, String((last.servo[1] & 0x7800) / 2048, DEC));
+  Heltec.display->drawString(94, 20, String((last.servo[1] & 0x07FF), DEC));
+  Heltec.display->drawString(20, 30, String((last.servo[2] & 0x7800) / 2048, DEC));
+  Heltec.display->drawString(30, 30, String((last.servo[2] & 0x07FF), DEC));
+  Heltec.display->drawString(84, 30, String((last.servo[3] & 0x7800) / 2048, DEC));
+  Heltec.display->drawString(94, 30, String((last.servo[3] & 0x07FF), DEC));
+  Heltec.display->drawString(20, 40, String((last.servo[4] & 0x7800) / 2048, DEC));
+  Heltec.display->drawString(30, 40, String((last.servo[4] & 0x07FF), DEC));
+  Heltec.display->drawString(84, 40, String((last.servo[5] & 0x7800) / 2048, DEC));
+  Heltec.display->drawString(94, 40, String((last.servo[5] & 0x07FF), DEC));
+  Heltec.display->drawString(20, 50, String((last.servo[6] & 0x7800) / 2048, DEC));
+  Heltec.display->drawString(30, 50, String((last.servo[6] & 0x07FF), DEC));
+  Heltec.display->display();
+  //delay(10);
+
+  if (temp > 100)
+    temp = 0;
+  else
+    temp++;
+
+  if (Serial1.available())
   {
-    //Serial.print("there has been a serial event \n \r ");
-    char temp = Serial1.read();
-    if (temp == 162)
+    while (Serial1.available())
     {
-      foundHeader = TRUE;
-      //Serial.print("\r");
-      //Serial.print(temp, DEC);
-      digitalWrite(14, HIGH);
-      delayMicroseconds(20);
-      digitalWrite(14, LOW);
-      delayMicroseconds(10);
-      for (int i = 0; i < 15; i++)
+      message[index] = Serial1.read();
+      if (message[index] == 0xA2)
       {
-        temp = Serial1.read();
-        //Serial.print(" ");
-        //Serial.print(temp, BIN);
-        digitalWrite(14, HIGH);
+        digitalWrite(17, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(17, LOW);
+        delayMicroseconds(10);
+        foundHeader = 1;
+      }
+      else
+      {
+        digitalWrite(17, HIGH);
         delayMicroseconds(5);
-        digitalWrite(14, LOW);
+        digitalWrite(17, LOW);
         delayMicroseconds(10);
       }
+      index++;
     }
-    else
-      return;
-
-    /*if (temp == 162)
-    
-      foundHeader = 1;
-      Serial.print(" the char was ");
-      Serial.print(temp, DEC);
-      Serial.print("\n\r");
-      Serial1.readBytes(inputString, 30);
-      Serial.print("Found a potential Header \n \r ");
-      Serial.print(" And this is the message ");
-      for (int i = 0; i < 30; i++)
-      {
-        Serial.print(inputString[i], DEC);
-        Serial.print(" ");
-      }
-      Serial.print("\n\r");
-      //foundHeader=0;
-      return;
-    }
-    else
+    last.fades = message[0];
+    last.system = message[1];
+    //last.servo[0] = message[2] * 256 + message[3];
+    index = 0;
+    while (index < 8)
     {
-      Serial.print("No header... returning \n \r ");
-      return;
-    }*/
+      // servo 0 use 2 and 3
+      // servo 1 use 4 and 5
+      // servo 2 use 6 and 7...
+      // servo 6 use 14 and 15...
+      last.servo[index] = message[(2 * index) + 2] * 256 + message[(2 * index) + 3];
+      index++;
+    }
+    // Finished reading
+
+    /*if (foundHeader)
+    {
+      Heltec.display->drawString(20, 10, String(last.fades, DEC));
+      Heltec.display->drawString(84, 10, String(last.system, HEX));
+      Heltec.display->drawString(20, 20, String(last.servo[0]));
+      Heltec.display->drawString(84, 20, String(last.servo[1]));
+      Heltec.display->drawString(20, 30, String(last.servo[2]));
+      Heltec.display->drawString(84, 30, String(last.servo[3]));
+      Heltec.display->drawString(20, 40, String(last.servo[4]));
+      Heltec.display->drawString(84, 40, String(last.servo[5]));
+      Heltec.display->drawString(20, 50, String(last.servo[6]));
+      Heltec.display->display();
+      delay(10);
+    }
+    foundHeader = 0;*/
+  }
+  if (Serial2.available()) // if date is coming from software serial port ==> data is coming from SoftSerial shield
+  {
+    while (Serial2.available()) // reading data into char array
+    {
+      buffer[count++] = Serial2.read(); // writing data into array
+      if (count == 64)
+        break;
+    }
+    Serial.write(buffer, count); // if no data transmission ends, write buffer to hardware serial port
+    clearBufferArray();          // call clearBufferArray function to clear the stored data from the array
+    count = 0;                   // set counter of while loop to zero 
   }
 }
